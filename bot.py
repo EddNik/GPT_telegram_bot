7f29c4bccac37196e4581b317c489735b62776c1
Eduard Vyskrebtsov <nikinna86@gmail.com>
2025-02-10 14:15:37 +0200

    gpt task

--- bot.py
+++ bot.py
@@ -8,13 +8,10 @@ from util import (load_message, send_text, send_image, show_main_menu, send_text
 
 import credentials
 
-MENU, OPTION1, OPTION2 = range(3)
 async def default_callback_handler(update: Update,
                                    context: ContextTypes.DEFAULT_TYPE):
     await update.callback_query.answer()
     query = update.callback_query.data
-    # if query == 'start':
-    #     await start(update, context)
     if query == "bot_random_btn_request":
         await random(update, context)
     elif query == "bot_random_btn_exit":
@@ -27,22 +24,8 @@ async def default_callback_handler(update: Update,
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     dialog.mode = "default"
-    print('start message')
-    # await update.callback_query.answer()
-    # command = update.callback_query.data
     text = load_message('main')
     await send_image(update, context, 'main')
-    keyboard = [
-        [InlineKeyboardButton("Option 1", callback_data="option1")],
-        [InlineKeyboardButton("Option 2", callback_data="option2")],
-        [InlineKeyboardButton("Option 2", callback_data="option2")],
-        [InlineKeyboardButton("Option 2", callback_data="option2")],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text(
-        "Welcome! Please choose an option:", reply_markup=reply_markup
-    )
-    
     await send_text(update, context, text)
     await show_main_menu(update, context, {
         'start': 'Головне меню',
@@ -53,58 +36,34 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         # Додати команду в меню можна так:
         # 'command': 'button text'
     })
-    return MENU
 
 # async def app_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("Random message")
     dialog.mode = "random"
     text = load_message('random')
     await send_image(update, context,'random')
     await send_text(update, context, text)
     prompt = load_prompt('random')
-    # content = await chat_gpt.send_question(prompt, 'Дай цікавий факт' )
-    # await send_text(update, context, content)
-
+    content = await chat_gpt.send_question(prompt, 'Дай цікавий факт' )
+    await send_text(update, context, content)
     text = load_message('thanksgiving')
-    print(text)
     content = await chat_gpt.send_question(text, 'Thank you')
-    print(text)
     await send_text_buttons(update, context, content,
                             {'bot_random_btn_request': 'Give me more facts', 'bot_random_btn_exit': 'Exit'})
 
 async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
     dialog.mode = "gpt"
-    # print('gpt message')
-
-    # await send_text(update, context, text)
-    # prompt = load_prompt('gpt')
-
-    text = load_message('gpt')
+    # text = load_message('gpt')
     await send_image(update, context, 'gpt')
-    await send_text_buttons(update, context, text,
-    #                         {'bot_gpt_btn_promt': 'Type your question again',       'bot_gpt_btn_exit': 'Exit'})
-                            {'bot_gpt_btn_exit': 'Exit'})
-    # message_type = update.message.chat.type
-    # text = update.message.text
-    # # await update.message.reply_text(text)
-    # time.sleep(15)
     text = update.message.text
-    await update.message.reply_text(f"You sent: {text}. Now send something else.")
-    # await update.message.reply_text('I am GPT please enter your request')
-    # time.sleep(5)
-
-    # print(text)
-    # await update.message.reply_text(text)
+    await update.message.reply_text(f"You sent: {text}. Wait for answer please.")
     await chat_gpt.add_message(text)
     answer = await chat_gpt.send_message_list()
-    # print(answer)
-    # user_text = update.message.from_user.username
     await send_text(update, context, answer)
+    await update.message.reply_text("Now type something else or exit.")
+    await send_text_buttons(update, context, text,
+                            {'bot_gpt_btn_exit': 'Exit'})
 
-    # content = await chat_gpt.send_question(prompt, 'Give my horoscope information')
-    # content = await chat_gpt.send_question(prompt, text)
-    # await send_text(update, context, content)
 
 async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
     dialog.mode = "talk"
